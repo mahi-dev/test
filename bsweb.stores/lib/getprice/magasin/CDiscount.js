6935cc62901b219b421468521ac85420d42eb2a4
Omar MAHI <o.mahi@outlook.com>
2017-04-14 16:08:03 +0200

    [Ajout Magasin][Auto]

--- bsweb.stores/lib/getprice/magasin/CDiscount.js
+++ bsweb.stores/lib/getprice/magasin/CDiscount.js
@@ -4,6 +4,7 @@ var price = document.querySelector("span.fpPrice.price");
 if (price != null) {
  return_price_value = price.textContent;
 }
+return_price_value = return_price_value.replace(/\s/g, "");
 return_price_value = return_price_value.replace("€", ".");
 return {
 price: return_price_value,
