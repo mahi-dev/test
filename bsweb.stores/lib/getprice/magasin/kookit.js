46ebe06d1c2f06fe7faf92314b3010ee83aab376
Omar MAHI <o.mahi@outlook.com>
2017-04-14 16:02:46 +0200

    [Ajout Magasin][Auto]

---
+++ bsweb.stores/lib/getprice/magasin/kookit.js
@@ -0,0 +1,13 @@
+window.get_pageinfo = function() {
+var return_price_value = "-2";
+var price = document.querySelector("div.prix-etiquette");
+if (price != null) {
+ return_price_value = price.textContent;
+}
+return_price_value = return_price_value.replace(/\s/g, "");
+return {
+price: return_price_value,
+stock: "",
+html: false,
+price_type: 2,
+img_url: ""};}
